16317eaee2eacb01bd0d8a1f1d77bd35b373489e
Soliprem <franci.solidoro@gmail.com>
2022-07-03 05:44:54 +0200

    layout

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 for file in "$HOME"/vimwiki/*.md
 do
   name=$(basename "$file" .md)
-  pandoc --template "$HOME"/vimwiki/site/layout/default.html "$file" -o "$HOME"/vimwiki/site/"$name".html
+  pandoc --standalone --include-in-header "$HOME/vimwiki/site/css/style.scss" --metadata pagetitle="$name" --template "$HOME"/vimwiki/site/layout/default.html -f markdown "$file" -t html --katex -o "$HOME"/vimwiki/site/"$name".html
 done
 
